60578683f1d3160cc325eb1ae2f238c9e5ef794e
Paul Carron <paulcarron@Pauls-MBP.home>
2021-11-10 17:08:04 +0000

    Add ImageService

---
+++ src/main/java/com/paulcarron/learningspringboot/learningspringboot/Image.java
@@ -0,0 +1,19 @@
+package com.paulcarron.learningspringboot.learningspringboot;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Image {
+
+    private int id;
+
+    private String name;
+
+    public Image(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
